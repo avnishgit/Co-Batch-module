1dd4b86a0ba36b4a90c67665faedf2e06b43839e
avnish <avnish84048@gmail.com>
2020-02-10 13:13:29 +0530

    First commit

---
+++ src/main/java/com/ag/CoBatchModuleApplication.java
@@ -0,0 +1,13 @@
+package com.ag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoBatchModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoBatchModuleApplication.class, args);
+	}
+
+}
